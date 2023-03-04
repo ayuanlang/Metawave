82e2bc37b9f559d9a2db96b1966e84be1b5e8a85
ayuanlang <125800414+ayuanlang@users.noreply.github.com>
2023-03-05 00:57:05 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -30,6 +30,9 @@ def talk():
     app.logger.info('hhhhhhhhhhhhhhhhhhhhh')
     data['ToUserName'] = params['FromUserName']
     data['FromUserName'] = params['ToUserName']
+    data['MsgType'] = params['text']
+    data['CreateTime'] = int(time.time())
+    
     data['Content'] = params['Content']
     app.logger.info(json.dumps(data))
     return make_succ_response(json.dumps(data))
